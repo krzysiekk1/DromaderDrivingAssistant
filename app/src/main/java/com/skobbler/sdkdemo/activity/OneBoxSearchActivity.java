50df9b93af46759d4692595ee1973942ce2b42e7
jcub731 <ksolawa@wp.pl>
2016-10-05 14:19:35 +0200

    OneBoxSearchActivity sketch

---
+++ app/src/main/java/com/skobbler/sdkdemo/activity/OneBoxSearchActivity.java
@@ -0,0 +1,24 @@
+package com.skobbler.sdkdemo.activity;
+
+import android.app.Activity;
+
+import com.skobbler.ngx.search.SKSearchListener;
+
+import java.util.List;
+
+/**
+ * Created by Jakub Solawa on 02.10.2016.
+ */
+
+public class OneBoxSearchActivity extends Activity implements SKSearchListener {
+
+    public void runOneLineSearch(){
+
+    }
+
+
+    @Override
+    public void onReceivedSearchResults(List list){
+
+    }
+}

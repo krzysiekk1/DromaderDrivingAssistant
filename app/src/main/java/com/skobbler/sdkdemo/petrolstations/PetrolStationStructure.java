2a4b11cada878a6ec064f0ad2c4403dd0ce149ed
msendera <marcin.sendera@gmail.com>
2016-12-02 16:46:27 +0100

    petrolstationstruct

--- app/src/main/java/com/skobbler/sdkdemo/petrolstations/PetrolStationStructure.java
+++ app/src/main/java/com/skobbler/sdkdemo/petrolstations/PetrolStationStructure.java
@@ -1,6 +1,9 @@
 package com.skobbler.sdkdemo.petrolstations;
 
 import com.skobbler.ngx.SKCoordinate;
+import com.skobbler.ngx.reversegeocode.SKReverseGeocoderManager;
+import com.skobbler.ngx.search.SKSearchResult;
+import com.skobbler.ngx.search.SKSearchResultParent;
 
 /**
  * Created by marcinsendera on 02.12.2016.
@@ -14,23 +17,53 @@ public class PetrolStationStructure {
     private SKCoordinate coordinates;
 
 
-    private double price;
+    // prices for fuels
+    private double petrol = 10.0;
+    private double diesel = 10.0;
+    private double lpg = 10.0;
 
     private String countryCode;
 
 
+    public PetrolStationStructure(int position, SKCoordinate coordinates){
+        this.positionNumber = position;
+        this.coordinates = coordinates;
 
-
-    public boolean compareCoordinates(){
-        return true;
-    }
-
-
-
-    private String getCountryCode(SKCoordinate coordinates){
         String countryName = "";
-        return countryName;
+        SKSearchResult result = SKReverseGeocoderManager.getInstance().reverseGeocodePosition(coordinates);
+        if (result != null && result.getParentsList() != null) {
+
+            for (SKSearchResultParent parent : result.getParentsList()) {
+                countryName = parent.getParentName();
+            }
+
+        }
+
+        this.countryCode = countryName;
+
+    }
+
+    //return true if the petrol station is the same, else return false
+    public boolean compareCoordinates(double longitude, double latitude){
+
+        if(this.coordinates.getLongitude() == longitude && this.coordinates.getLatitude() == latitude){
+            return true;
+        }
+
+        return false;
     }
 
 
+
+    public void setPetrolCost(double price){
+        this.petrol = price;
+    }
+
+    public void setDieselCost(double price){
+        this.petrol = price;
+    }
+
+    public void setLpgCost(double price){
+        this.petrol = price;
+    }
 }

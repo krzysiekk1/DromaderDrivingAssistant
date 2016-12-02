54e91e72db3f2095a8dab97049f084ec3a07091e
msendera <marcin.sendera@gmail.com>
2016-12-02 16:21:28 +0100

    gasstationstruct

--- app/src/main/java/com/skobbler/sdkdemo/fuel/GasStationStructure.java
+++ app/src/main/java/com/skobbler/sdkdemo/fuel/GasStationStructure.java
@@ -1,8 +1,31 @@
 package com.skobbler.sdkdemo.fuel;
 
+import com.skobbler.ngx.SKCoordinate;
+
 /**
  * Created by marcinsendera on 02.12.2016.
  */
 
 public class GasStationStructure {
+
+    // initializing my
+    private int positionNumber;
+
+    private SKCoordinate coordinates;
+
+
+    private double price;
+
+    private String countryCode;
+
+
+
+
+
+    private String getCountryCode(SKCoordinate coordinates){
+        String countryName = "";
+        return countryName;
+    }
+
+
 }
